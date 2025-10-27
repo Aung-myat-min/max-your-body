1760e1aae575147116999139290a6af6cc9f0396
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-10-27 15:06:56 +0000

    feat: mid way of something

---
+++ src/controllers/objects/exercise.controller.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+
+// GET EXERCISES
+// GET ONE EXERCISE WITH ITS RELATED RECORDS
+// CREATE EXERCISE
+// UPDATE EXERCISE
+// DELETE EXERCISE
--- src/controllers/objects/profile.controller.ts
+++ src/controllers/objects/profile.controller.ts
@@ -21,6 +21,55 @@ export async function getProfile(userId: string): Promise<ResponseHelper<Profile
 }
 
 // CREATE PROFILE
+export async function createProfile({userId, profileImg, weight, height}: Partial<Profile>): Promise<ResponseHelper<Profile| null>>{
+    let response: ResponseHelper<Profile | null>;
+    try {
+        const profile = await prisma.profile.create({
+            data: {
+                profileImg: profileImg!,
+                weight: weight!,
+                height: height!,
+                userId: userId!,
+            }});
+
+        response = ResponseHelper.Success(profile, "Here is the new profile!");
+    } catch (e) {
+        console.error("Error getting profile: ",e);
+        response = ResponseHelper.ServerErr("Error while creating profile");
+    }
+    return response
+}
+
 // UPDATE PROFILE
-// DELETE PROFILE
+export async function updateProfile({userId, profileImg, weight, height}: Partial<Profile>): Promise<ResponseHelper<Profile| null>>{
+    let response: ResponseHelper<Profile | null>;
+    try {
+        const profile = await prisma.profile.update({
+            where: {userId: userId},
+            data: {
+                profileImg: profileImg,
+                weight: weight,
+                height: height,
+            }});
+
+        response = ResponseHelper.Success(profile, "Here is the updated profile!");
+    } catch (e) {
+        console.error("Error getting profile: ",e);
+        response = ResponseHelper.ServerErr("Error while updating profile");
+    }
+    return response
+}
+
+// DELETE PROFILE FEATURE doesn't exist because of the relation with the user column.
+
 // UPLOAD USER PROFILE
+// export async function (userId: string): Promise<ResponseHelper<Profile| null>>{
+//     let response: ResponseHelper<Profile | null>;
+//     try {
+//
+//     } catch (e) {
+//         console.error("Error getting profile: ",e);
+//         response = ResponseHelper.ServerErr("Error while getting profile");
+//     }
+//     return response
+// }
